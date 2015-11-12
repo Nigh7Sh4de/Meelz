e74fee4cb1c6276a3294722cb35323674065292d
Nigh7Sh4de <plotnikd@gmail.com>
2015-11-12 10:52:56 -0500

    Add create foot item functionality

--- src/createfoodpage.js
+++ src/createfoodpage.js
@@ -1,8 +1,20 @@
 var CreateFoodPage = React.createClass({
+    handleClick: function(event) {
+        event.preventDefault();
+        _foods.push({
+            "name": this.refs.name.value
+        });
+        redraw(Pages.FoodItemListPage);
+    },
     render: function() {
         return (
             <div>
-                Create new Food
+                <input ref="name" defaultValue="name" /><br />
+                Calories: <input ref="calories" defaultValue="0" /><br />
+                Protein: <input defaultValue="0" /><br />
+                Sugar: <input defaultValue="0" /><br />
+                Carbs: <input defaultValue="0" /><br />
+                <button onClick={this.handleClick}>+ Create</button>
             </div>
         );
     }
