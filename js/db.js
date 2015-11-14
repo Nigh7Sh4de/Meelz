9b5cc76e48ac6d837363de928b428795ac52bcef
Nigh7Sh4de <plotnikd@gmail.com>
2015-11-14 13:08:05 -0500

    Change CurrentDay structure

---
+++ js/db.js
@@ -0,0 +1,44 @@
+var _propTypes = ["text", "number"];
+
+var _props = [
+// {
+//     "name": "name",
+//     "type": "text"
+// }
+"name" //,
+// "calories",
+// "protein",
+// "carbs",
+// "sugar"
+];
+
+var _days = [{
+    "id": 0,
+    "food": [0, 1, 2],
+    "archive": true
+}, {
+    "id": 1,
+    "food": [0],
+    "archive": true
+}, {
+    "id": 2,
+    "food": [1, 3],
+    "archive": true
+}, {
+    "id": 3,
+    "food": []
+}];
+
+var _foods = [{
+    "id": 0,
+    "name": "Mashed Potatoes"
+}, {
+    "id": 1,
+    "name": "Chicken Breast"
+}, {
+    "id": 2,
+    "name": "Vegetables"
+}, {
+    "id": 3,
+    "name": "Spaghetti"
+}];
