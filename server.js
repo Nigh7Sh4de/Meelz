69dc6d02d3bee062251c3268ed8553cd08b943f8
Nigh7Sh4de <plotnikd@gmail.com>
2016-01-20 13:16:03 -0500

    Add server.js start point for nodejs

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var http = require('http');
 var express = require('express');
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    res.sendFile('index.htm', {root: __dirname + '/public/'});
+    res.sendfile('index.htm', {root: __dirname + '/public/'});
 });
 
-http.createServer(app).listen(8888);
+http.createServer(app).listen(server_port, server_ip_address);
+console.log(server_port);
